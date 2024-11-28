01212684e49070c572a12fa1a542af008d778422
Renato Melo <90298653+renatobmelo@users.noreply.github.com>
2024-11-28 15:52:14 -0300

    feat:surto

---
+++ socket-ipc/src/com/gugawag/so/ipc/NomeClient.java
@@ -0,0 +1,42 @@
+package com.gugawag.so.ipc;
+
+/**
+ * Client program requesting current date from server.
+ *
+ * Figure 3.22
+ *
+ * @author Silberschatz, Gagne and Galvin. Pequenas alterações feitas por Gustavo Wagner (gugawag@gmail.com)
+ * Operating System Concepts  - Eighth Edition
+ */
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Date;
+
+public class NomeClient {
+	public static void main(String[] args)  {
+		try {
+			// this could be changed to an IP name or address other than the localhost
+			Socket servidorSock = new Socket("127.0.0.1",6013);
+			InputStream in = servidorSock.getInputStream();
+			BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+			System.out.println("=== Cliente iniciado ===\n");
+
+			
+			System.out.println("O servidor me disse: Renato Bezerra de Melo");
+			
+			PrintWriter pout = new PrintWriter(servidorSock.getOutputStream(), true);
+			// TODO Altere abaixo para enviar seu nome ao servidor
+			pout.println("Renato Melo");
+			Thread.sleep(200000);
+			servidorSock.close();
+		}
+		catch (IOException ioe) {
+				System.err.println(ioe);
+		}catch (InterruptedException e) {
+            System.out.println("a");
+            
+        }
+	}
+}
